d715f389a590598fd932e3350d411d2da4a1bcb4
tanimSrabon13 <inamul.ke@gmail.com>
2022-01-12 17:56:24 +0600

    snakBar Add

--- lib/details.dart
+++ lib/details.dart
@@ -7,11 +7,25 @@ class DetailsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    home: Scaffold(appBar: AppBar(title: Text('Details Page'),
+    home: Scaffold(appBar:
+    AppBar(
+      title: Text('Details Page'),
       ),
-     body: Column(children: [
-       Expanded(child: SvgPicture.asset('assets/images/svgpic.svg')),
-     ],),
+     body: Column(
+       children: [
+          Expanded(child: SvgPicture.asset('assets/images/svgpic.svg')),
+         Center(
+           child: Builder(builder: (context)=>RaisedButton(onPressed: (){
+             Scaffold.of(context).showSnackBar(
+               SnackBar(behavior: SnackBarBehavior.floating,
+                 content: Text('submit completed'),),);
+           },
+           child: Text('SnackBar'),),),
+         ),
+         
+       ],
+     ),
+     
     ),
     );
   }
